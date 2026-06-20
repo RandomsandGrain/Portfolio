a97e41c9a7b13cd23a0ba59f3aa3bbad012515ce
Esteban Jennings <estebanjennings2008@gmail.com>
2026-06-20 02:53:46 -0500

    portfolio

---
+++ app/about/page.tsx
@@ -0,0 +1,30 @@
+
+
+function About() {
+    return <div className="flex flex-col items-center space-y-8">
+    <p className="text-3xl mt-10 text-center">My Name is Esteban Jennings.
+        <br/> 
+        I'm a frontend web developer building apps with techs like React, and TypeScript.
+        </p>
+
+    <p className="font-bold text-3xl">Tech: </p>
+    <ul className="text-xl border border-white p-8 text-green-500 font-bold">
+      <li>React</li>
+      <li>TypeScript</li>
+      <li>Next.js</li>
+      <li>JavaScript</li>
+      <li>Tailwind CSS</li>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>Git & GitHub</li>
+    </ul>
+
+    <p className="text-2xl">
+  I'm focused on improving my frontend skills and working toward my
+  first software development role.
+   </p>
+
+    </div>
+}
+
+export default About
---
+++ app/projects/page.tsx
@@ -0,0 +1,129 @@
+
+
+function Projects() {
+    return <div>
+
+       <div className="m-10 flex">
+            <img className="h-96 w-1/2" src="/projects/JobTracker2.0.png" alt="Job-Tracker2.0 Project"/>
+               <div className="w-1/2 ml-5 space-y-4">
+             <p className="font-bold text-3xl">JobTracker2.0</p>
+             <p>A job tracking app that pulls listings from an API and lets users search, add jobs through a form, and saves them using localStorage.</p>
+             <p className="text-green-500 font-bold text-xl">Tech:  React, React Router, Typescript, Tailwind CSS, API</p>
+             <a
+             target="_blank"
+             rel="noopener noreferrer"
+             href="https://estebans-job-tracker.vercel.app/"
+             className="text-blue-400 text-xl hover:underline mr-10"
+             >
+            Live Demo   
+             </a>
+             <a 
+             className="text-gray-400 hover:underline text-xl" 
+             href="https://github.com/RandomsandGrain/Estebans-Job-Tracker2.0" 
+             rel="noopener noreferrer" 
+             target="_blank">
+            Github
+             </a>
+               </div>     
+       </div>
+
+       <div className="m-10 flex">
+            <img className="h-96 w-1/2" src="/projects/Esteban-indeed.png" alt="Esteban-indeed Project"/>
+               <div className="w-1/2 ml-5 space-y-4">
+             <p className="font-bold text-3xl">Esteban-indeed</p>
+             <p>A job board style app where users can browse and filter job listings, view job details, and navigate between pages using React Router.</p>
+             <p className="text-green-500 font-bold text-xl">Tech: React, React Router, Tailwind CSS, API</p>
+             <a
+             target="_blank"
+             rel="noopener noreferrer"
+             href="https://esteban-s-indeed.vercel.app/"
+             className="text-blue-400 text-xl hover:underline mr-10"
+             >
+            Live Demo   
+             </a>
+             <a 
+             className="text-gray-400 hover:underline text-xl" 
+             href="https://github.com/RandomsandGrain/Esteban-s-Indeed" 
+             rel="noopener noreferrer" 
+             target="_blank">
+            Github
+             </a>
+               </div>     
+       </div>
+
+        <div className="m-10 flex">
+            <img className="h-96 w-1/2" src="/projects/movie-search.png" alt="movie-search Project"/>
+               <div className="w-1/2 ml-5 space-y-4">
+             <p className="font-bold text-3xl">Movie-Search</p>
+             <p>A movie search app that fetches and displays movies from an API, with routing for individual movie details and dynamic pages.</p>
+             <p className="text-green-500 font-bold text-xl">Tech: React, React Router, API</p>
+             <a
+             target="_blank"
+             rel="noopener noreferrer"
+             href="https://movie-search-eta-two.vercel.app/"
+             className="text-blue-400 text-xl hover:underline mr-10"
+             >
+            Live Demo   
+             </a>
+             <a 
+             className="text-gray-400 hover:underline text-xl" 
+             href="https://github.com/RandomsandGrain/movie-search" 
+             rel="noopener noreferrer" 
+             target="_blank">
+            Github
+             </a>
+               </div>     
+       </div>
+
+        <div className="m-10 flex">
+            <img className="h-96 w-1/2" src="/projects/Todo.png" alt="Todo-list Project"/>
+               <div className="w-1/2 ml-5 space-y-4">
+             <p className="font-bold text-3xl">Todo-List</p>
+             <p> A simple todo app where users can add, complete, and delete tasks, with state management using React hooks.</p>
+             <p className="text-green-500 font-bold text-xl">Tech: React</p>
+             <a
+             target="_blank"
+             rel="noopener noreferrer"
+             href="https://todo-list-xi-lime.vercel.app/"
+             className="text-blue-400 text-xl hover:underline mr-10"
+             >
+            Live Demo   
+             </a>
+             <a 
+             className="text-gray-400 hover:underline text-xl" 
+             href="https://github.com/RandomsandGrain/Todo-List" 
+             rel="noopener noreferrer" 
+             target="_blank">
+            Github
+             </a>
+               </div>     
+       </div>
+
+        <div className="m-10 flex">
+            <img className="h-96 w-1/2" src="/projects/random-joke-viewer.png" alt="random-joke-viewer Project"/>
+               <div className="w-1/2 ml-5 space-y-4">
+             <p className="font-bold text-3xl">Random-Joke-Viewer</p>
+             <p>An app that fetches random jokes from an API and displays them, with a button to load a new joke each time.</p>
+             <p className="text-green-500 font-bold text-xl">Tech: React, API</p>
+             <a
+             target="_blank"
+             rel="noopener noreferrer"
+             href="https://random-joke-viewer.vercel.app/"
+             className="text-blue-400 text-xl hover:underline mr-10"
+             >
+            Live Demo   
+             </a>
+             <a 
+             className="text-gray-400 hover:underline text-xl" 
+             href="https://github.com/RandomsandGrain/random-joke-viewer" 
+             rel="noopener noreferrer" 
+             target="_blank">
+            Github
+             </a>
+               </div>     
+        </div>
+
+    </div>
+}
+
+export default Projects
